a80909dc718e0807f84114f31db08eaf36a96616
moheie <109584504+moheie@users.noreply.github.com>
2024-11-28 14:22:03 +0200

    fixed that front can listen to backend in browser ( fixed what adham could not again )

--- run.sh
+++ run.sh
@@ -1,29 +1,26 @@
-# networks 
-docker network create front
+#!/bin/bash
 
+echo "Creating Docker networks"
+docker network create front
 docker network create database
 
-# front image
+echo "Starting the frontend container"
 docker build -t frontend ./front-end
-
 docker run -d --rm -p 4200:4200 --net front --name frontendContainer frontend
 
-# database image
+echo "Starting the database container"
 docker build -t database ./database
-
 docker run -d --rm --net database --name databaseContainer database
+sleep 10
 
-# backend image
+echo "Starting the backend container"
 docker build --build-arg DB_CONTAINER=databaseContainer -t backend ./back-end
-
 docker run -d --rm --net database --net front --name backendContainer backend
+sleep 5
 
-# nginx image
+echo "Starting the Nginx reverse proxy"
 docker build -t nginx-proxy ./nginx
-
 docker run -d --rm -p 8080:80 --net front --net database --name nginxContainer nginx-proxy
 
-# ps 
-
+echo "All containers are up and running!"
 docker ps
-
