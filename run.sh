5c2a1482967b6be34cc265a27d23aded1e2156d5
Adham Aboelela <100962253+AdhamIsHere@users.noreply.github.com>
2024-12-23 22:23:01 +0200

    fixed permission issue in front

--- run.sh
+++ run.sh
@@ -9,16 +9,16 @@ docker volume create MysqlData
 
 echo "Starting the frontend container"
 docker build -t frontend ./front-end
-docker run -d --rm -p 4200:4200 --net front --name frontendContainer frontend
+docker run -d --rm -p 4200:4200 --net front --name frontend frontend
 
 echo "Starting the database container"
 docker build -t database ./database
-docker run -d --rm --net database --name databaseContainer -v MysqlData:/var/lib/mysql database
+docker run -d --rm --net database --name database -v MysqlData:/var/lib/mysql database
 sleep 10
 
 echo "Starting the backend container"
-docker build --build-arg DB_CONTAINER=databaseContainer -t backend ./back-end
-docker run -d --rm --net database --net front -p 8080:8080 --name backendContainer backend
+docker build -t backend ./back-end
+docker run -d --rm --net database --net front -p 8080:8080 --name backend backend
 sleep 5
 
 echo "All containers are up and running!"
